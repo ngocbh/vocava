88dcfe031ba33ea8c4d4380db361d8eed3c4842d
jasoncin <quoccbinh@gmail.com>
2019-08-14 18:16:24 +0700

    User sentence

--- server/routes/ocr.py
+++ server/routes/ocr.py
@@ -24,7 +24,9 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return ocr_text.get_text(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            result =  ocr_text.get_text(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print("Finished.")
+            return result
         else:
             return {"Null"}
 
